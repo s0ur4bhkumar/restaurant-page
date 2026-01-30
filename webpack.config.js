799f917c3447e6ef06524ea23008585b910eef87
s0ur4bhkumar <soul65917@gmail.com>
2026-01-30 15:07:45 +0530

    add background image

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,10 @@ export default {
         test: /\.html$/i,
         use: ["html-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
