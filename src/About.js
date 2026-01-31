112329f8f6f70d329e1379ddaf417365270df08d
s0ur4bhkumar <soul65917@gmail.com>
2026-01-31 11:54:20 +0530

    fix with of about info

--- src/About.js
+++ src/About.js
@@ -2,6 +2,7 @@
   const container = document.getElementById("container");
   container.style.alignItems = 'center';
   container.style.width = '100%';
+  container.style.padding = '0';
   const aboutBtn = document.querySelector(".About");
   const homeBtn = document.querySelector(".Home");
   const menuBtn = document.querySelector(".Menu");
